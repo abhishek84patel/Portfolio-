e155de49eaa07d52ba1fc4ac5bc74e65cda2ce4b
Abhishek Patel <108073387+abhishek84patel@users.noreply.github.com>
2023-07-21 15:12:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const menu = document.getElementById("nav");
+const m_icon = document.getElementById("m-img");
+console.log(menu)
+
+function showMenu() {
+    
+
+    menu.classList.toggle("show");
+
+
+}
